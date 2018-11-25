29d8ef83af4a2786498faa99c84791fd115d98cc
Ivan Petrov <ivan.antonov.p@gmail.com>
2018-11-25 13:02:35 +0200

    initial UserService commit

---
+++ api/services/UserService.js
@@ -0,0 +1,19 @@
+
+
+createUser = async function (tokens) {
+    return 'NOT IMPLEMENTED';
+}
+
+storeTransactions = async function (userId, tokens) {
+    return 'NOT IMPLEMENTED';
+}
+
+getTransactions = async function (userId) {
+
+}
+
+module.exports = {
+    createUser,
+    storeTransactions,
+    getTransactions
+}
