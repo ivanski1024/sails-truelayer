4e84eff033eee408f6743d42f80a6987ef109a90
Ivan Petrov <ivan.antonov.p@gmail.com>
2018-11-25 13:40:12 +0200

    AppController proper returns

--- api/controllers/AppController.js
+++ api/controllers/AppController.js
@@ -1,16 +1,8 @@
-/**
- * AppController
- *
- * @description :: Server-side actions for handling incoming requests.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
-
 const config = sails.config.custom;
 const TrueLayerService = sails.services.truelayerservice;
 const UserService = sails.services.UserService;
 
 const redirectUrl = config.baseUrl + 'callback';
-
 
 module.exports = {
   handleRoot: function(req, res) {
@@ -22,27 +14,36 @@ module.exports = {
     try {
       let tokens = await TrueLayerService.exchangeCodeForTokens(redirectUrl, req.query.code);
       let userId = await UserService.createUser(tokens);
-      let transactions = await TrueLayerService.fetchTransactions(tokens);
-      let result = await UserService.storeTransactions(userId, transactions)
-      return result;
+
+      // fetch and store bank transactions
+      let bankTransactions = await TrueLayerService.fetchBankTransactions(tokens);
+      await UserService.storeTransactions(userId, bankTransactions)
+
+      // fetch and store card transactions
+      let cardTransactions = await TrueLayerService.fetchCardTransactions(tokens);
+      await UserService.storeTransactions(userId, cardTransactions);
+
+      return res.ok({ status: 'Success', userId: userId });
     } catch (err) {
-      return res.serverError(err);
+      return res.serverError({ status: 'Failed', error: err });
     }
   },
 
   handleGetTransactions: async function (req, res) {
     try {
-      return await UserService.fetchTransactions(req.query.userId);
+      let transactions = await UserService.fetchTransactions(req.query.userId);
+      return res.ok({ status: 'Success', results: transactions })
     } catch (err) {
-      return res.serverError(err);
+      return res.serverError({ status: 'Failed', error: err });
     }
   },
 
   handleGetDebugInformation: async function (req, res) {
     try {
-      return await UserService.fetchDebugInformation(req.query.userId);
+      let debugInformation = await UserService.fetchDebugInformation(req.query.userId);
+      return res.ok({ status: 'Success', results: debugInformation })
     } catch (err) {
-      return res.serverError(err);
+      return res.serverError({ status: 'Failed', error: err });
     }
   }
 };
